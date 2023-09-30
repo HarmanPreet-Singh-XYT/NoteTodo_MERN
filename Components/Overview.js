9ee68917a4eb3e2168c08b7febbec3fe7238378c
Harman <preetsinghharman27@gmail.com>
2023-09-30 22:12:32 +0530

    Fix Sidebar Deselect Click Bug

--- Components/Overview.js
+++ Components/Overview.js
@@ -1,23 +1,15 @@
-import React,{useContext} from 'react'
+import React from 'react'
 import Calendar from './Calender'
-import TodoGraph from './Output/TodoGraph'
-import NotesGraph from './Output/NotesGraph'
-import { TodoStatus } from './Output/TodoStatus'
-import NotesStatus from './Output/NotesStatus'
-import { Number_cont } from '@/Helpers/Numbers-Status'
 import To_No_Count from './Overview/To-No-Count'
 import Graphs from './Overview/Graphs'
 import Storage from './Overview/Storage'
 import Account from './Overview/Account'
 const Overview = () => {
-    // const time = new Date();
-    const {TodoProgress,TodoDone,NotesDone,NotesProgress} = useContext(Number_cont);
   return (
     <>
     <div className='ov-nav'></div>
     <main className="ov-container">
         <div className="ov-insider o1 ov-box">
-            {/* <div className='ov-time'>{time.getHours()}:{time.getMinutes()}</div> */}
             <Calendar/>
         </div>
         <div className="ov-insider o2 ov-box">
--- Components/PageComponents/Sidebar-options.js
+++ Components/PageComponents/Sidebar-options.js
@@ -6,12 +6,12 @@ const SidebarOptions = () => {
     <>
     <hr className="hrline"/>
     <section className="options">
-                <button name='overview' onClick={(e)=>setSelectedButton(e.target.name)} className={selectedButton==="overview" ? "overview bt side-selected-btn" : "overview bt"}><span name='overview' className="lg"><i className="fa-solid fa-city"></i></span>Overview</button>
-                <button name='task' onClick={(e)=>setSelectedButton(e.target.name)} className={selectedButton==="task" ? "task bt side-selected-btn" : "task bt"}><span name='task' className="lg"><i name='task' className="fa-solid fa-list"></i></span>Tasks</button>
-                <button disabled name='document' onClick={(e)=>setSelectedButton(e.target.name)} className={selectedButton==="document" ? "document bt side-selected-btn" : "document bt"}><span className="lg"><i className="fa-solid fa-folder-open"></i></span>Documents</button>
-                <button name='note' onClick={(e)=>setSelectedButton(e.target.name)} className={selectedButton==="note" ? "note bt side-selected-btn" : "note bt"}><span className="lg"><i className="fa-solid fa-pen-to-square"></i></span>Notes</button>
-                <button name='output' onClick={(e)=>setSelectedButton(e.target.name)} className={selectedButton==="output" ? "output bt side-selected-btn" : "output bt"}><span className="lg"><i className="fa-solid fa-fire"></i></span>Output</button>
-                <button name='support' onClick={(e)=>setSelectedButton(e.target.name)} className={selectedButton==="support" ? "support bt side-selected-btn" : "support bt"}><span className="lg"><i className="fa-solid fa-comments"></i></span>Support</button>
+                <button name='overview' onClick={()=>setSelectedButton("overview")} className={selectedButton==="overview" ? "overview bt side-selected-btn" : "overview bt"}><span name='overview' className="lg"><i name='overview' className="fa-solid fa-city"></i></span>Overview</button>
+                <button name='task' onClick={()=>setSelectedButton("task")} className={selectedButton==="task" ? "task bt side-selected-btn" : "task bt"}><span name='task' className="lg"><i id='task' className="fa-solid fa-list"></i></span>Tasks</button>
+                <button disabled name='document' onClick={()=>setSelectedButton("document")} className={selectedButton==="document" ? "document bt side-selected-btn" : "document bt"}><span className="lg"><i id='document' className="fa-solid fa-folder-open"></i></span>Documents</button>
+                <button name='note' onClick={()=>setSelectedButton("note")} className={selectedButton==="note" ? "note bt side-selected-btn" : "note bt"}><span className="lg"><i id='note' className="fa-solid fa-pen-to-square"></i></span>Notes</button>
+                <button name='output' onClick={()=>setSelectedButton("output")} className={selectedButton==="output" ? "output bt side-selected-btn" : "output bt"}><span className="lg"><i id='output' className="fa-solid fa-fire"></i></span>Output</button>
+                <button name='support' onClick={()=>setSelectedButton("support")} className={selectedButton==="support" ? "support bt side-selected-btn" : "support bt"}><span className="lg"><i id='support' className="fa-solid fa-comments"></i></span>Support</button>
     </section>
     <hr className="hrline"/>
     </>
