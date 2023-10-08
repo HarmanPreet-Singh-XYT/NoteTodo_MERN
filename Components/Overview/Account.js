a3b79262a6e965394a0b70e279d9f8891150e031
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2023-10-08 17:22:53 +0530

    Add Complete Todo Sync With DB

--- Components/Overview/Account.js
+++ Components/Overview/Account.js
@@ -3,13 +3,16 @@ import { Account_cont } from '@/Helpers/Account-Info'
 import React, { useContext } from 'react'
 
 const Account = () => {
-    const {AccountInfo} = useContext(Account_cont);
+    const {AccountInfo,setAccountInfo} = useContext(Account_cont);
+    function ProfileEdit(e){
+        e.preventDefault();
+    }
   return (
     <>
         <main className="ov-spt-container">
             <div className="ov-spt-window">
                 <div className="ov-spt-content">
-                    <form className="ov-spt-form" id="edit-form" action="/account/edit" method="post">
+                    <form onSubmit={(e)=>ProfileEdit(e)} className="ov-spt-form" id="edit-form" action="/account/edit" method="post">
                         <label className="ov-spt-title">Name</label>
                         <input defaultValue={AccountInfo.name} name='name' placeholder="Name" type="text" className="ov-spt-in-title" />
                         <label className="ov-spt-title">Bio</label>
--- routes/Todo-edit.js
+++ routes/Todo-edit.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {userTodo} = require('../data/Data.js');
 const { authenticateToken } = require('../data/Auth.js');
-router.post('/todo/edit',(req,res)=>{
-
+router.patch('/todo/edit',authenticateToken,(req,res)=>{
+    userTodo.findOneAndUpdate({User_id:req.body.User_id,id:req.body.id},req.body)
+    .exec()
+    .then((Response)=>{
+        res.status(200).json({message:'Success'})
+    })
+    .catch((err)=>{
+        res.status(500).json({message:'Error',error:err})
+    })
 })
 module.exports=router;
