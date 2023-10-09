a03ad2ba07cbaa6a37fcc2add0a47945019966fc
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2023-10-09 14:55:22 +0530

    Add Complete Profile Edit Support

--- routes/Edit-Account.js
+++ routes/Edit-Account.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { authenticateToken } = require('../data/Auth.js');
 const {userData} = require('../data/Data.js');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 router.patch('/account/edit',authenticateToken,(req,res)=>{
     const data = {
         name:req.body.name,
@@ -33,5 +35,19 @@ router.patch('/account/reset',authenticateToken,(req,res)=>{
     .catch((err)=>{
         res.status(500).json({message:'Error',error:err})
     })
-})
+});
+router.patch('/account/password',authenticateToken,(req,res)=>{
+    const hash = bcrypt.hashSync(req.body.password, saltRounds);
+    const data = {
+        password:hash,
+    }
+    userData.findOneAndUpdate({User_id:req.headers.user_id},data)
+    .exec()
+    .then((Response)=>{
+        res.status(200).json({message:'Success'})
+    })
+    .catch((err)=>{
+        res.status(500).json({message:'Error',error:err})
+    })
+});
 module.exports=router;
