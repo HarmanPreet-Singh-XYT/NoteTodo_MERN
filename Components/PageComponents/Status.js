7ed8ee2f5298961896028b0456f3ad3f4082c86e
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2023-10-10 20:34:02 +0530

    Add Logout Option/Add Category Create Option

--- Components/PageComponents/Status.js
+++ Components/PageComponents/Status.js
@@ -1,13 +1,19 @@
 import { FormData_cont } from '@/Helpers/CardCreationData';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 const Status = () => {
     const {months} = useContext(FormData_cont);
+    const [dropdown, setdropdown] = useState(false);
     const date = new Date;
   return (
     <>
         <section className="status">
                 <div className="current-date"><h1 className="date-insider">{date.getDate()} {" "} {months[date.getMonth()]} {" "} {date.getFullYear()}</h1></div>
-                <div className="settings"><i className="fa-solid fa-gear"></i></div>
+                <div class="dropdown">
+                <button class="dropbtn"><i class="fa-solid fa-gear"></i></button>
+                <div class="dropdown-content">
+                  <a onClick={()=>location.reload()} >Logout</a>
+                </div>
+              </div>
         </section>
     </>
   )
