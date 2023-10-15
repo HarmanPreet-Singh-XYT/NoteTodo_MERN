1135de497f1a9099c6cc4480d52e3ac6d199ba6f
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2023-10-16 03:13:52 +0530

    Add Links Environment Variables

--- Components/PageComponents/Links.js
+++ Components/PageComponents/Links.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 const Links = () => {
+  const Linkedin = NEXT_PUBLIC_LINKEDIN;
+  const Discord = NEXT_PUBLIC_DISCORD;
+  const Github = NEXT_PUBLIC_GITHUB;
+  const Email = NEXT_PUBLIC_EMAIL;
   return (
     <>
         <hr className="hrline"/>
                 <section className="links">
-                    <a className="li" href="https://www.linkedin.com/in/harmanpreet-singh-016579228/"><i className="fa-brands fa-linkedin"></i></a>
-                    <a className="li" href="https://discord.gg/wQ9dVqwPzx"><i className="fa-brands fa-discord"></i></a>
-                    <a className="li" href="https://github.com/HarmanPreet-Singh-XYT"><i className="fa-brands fa-github"></i></a>
-                    <a className="li" href=""><i className="fa-solid fa-envelope"></i></a>
+                    <a className="li" href={Linkedin}><i className="fa-brands fa-linkedin"></i></a>
+                    <a className="li" href={Discord}><i className="fa-brands fa-discord"></i></a>
+                    <a className="li" href={Github}><i className="fa-brands fa-github"></i></a>
+                    <a className="li" href={Email}><i className="fa-solid fa-envelope"></i></a>
                 </section>
         <hr className="hrline"/>
     </>
